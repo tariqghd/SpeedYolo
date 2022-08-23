0adc3e7e293177fcac5cd478816f3eab7d576331
Tariq Daradkeh <tariqghd@gmail.com>
2022-08-23 16:26:21 -0400

    vv

--- yolo_submit.sh
+++ yolo_submit.sh
@@ -1,24 +1,24 @@
 #!/encs/bin/tcsh
-
+## since it is qlogin no need to configure cluster setting because qlogin choosed the proper computational node  
 # Give job a name
-#$ -N yolo
+# #$ -N yolo
 
 # Set output directory to current
-#$ -cwd
+# #$ -cwd
 
 # Send an email when the job starts, finishes or if it is aborted.
-#$ -m bea
+# #$ -m bea
 
 # Request GPU
 #  #$ -l gpu=2
 
 # Request CPU with maximum memoy size = 40GB
-#$ -l h_vmem=40G
+# #$ -l h_vmem=40G
 
 #sleep 30
 
 # Specify the output file name in our case we commntes that system will genreate file with the same name of the job
-# -o name.qlog
+# # -o name.qlog
 
 
 conda activate /speed-scratch/$USER/YOLOInteractive
